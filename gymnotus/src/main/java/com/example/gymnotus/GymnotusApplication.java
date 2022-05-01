57007e65dc812ead013a6ed90b75d21cdaf64061
Tomasz Toporowski <tomasztoporowski52@gmail.com>
2022-05-01 07:41:17 +0200

    initial version

---
+++ gymnotus/src/main/java/com/example/gymnotus/GymnotusApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymnotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnotusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymnotusApplication.class, args);
+    }
+
+}
